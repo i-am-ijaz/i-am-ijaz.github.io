4cf0b169b2d0e616c35b8ad4b1de29b26bac5951
ijazsharif <ijazsharif34@gmail.com>
2021-07-13 10:00:17 +0500

    updated api

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ document.addEventListener("DOMContentLoaded", () => {
 
     async function updateQuote() {
         // Fetch a random quote from the Quotable API
-        const response = await fetch("https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json");
+        const response = await fetch("https://quotable.io/random");
         const data = await response.json();
         if (response.ok) {
             // Update DOM elements
-            quote.textContent = data.quoteText;
-            cite.textContent = data.quoteAuthor;
+            quote.textContent = data.content;
+            cite.textContent = data.author;
         } else {
             quote.textContent = "An error occured";
             console.log(data);
